3e5ad100c8f47a6003fe37fcf0a8f78f1b9c87a6
Nirklav <mrAnton911@gmail.com>
2016-09-24 16:56:31 +0300

    Assembly info changes;

--- BinSerializer/Properties/AssemblyInfo.cs
+++ BinSerializer/Properties/AssemblyInfo.cs
@@ -1,18 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BinSerializer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Binary serializer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ThirtyNineEighty")]
 [assembly: AssemblyProduct("BinSerializer")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © ThirtyNineEighty 2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("Invariant")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
