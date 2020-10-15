662cfc39f2e6fbcef791699eb60b7104671238a5
王雪飞 <wxf909149916@hotmail.com>
2020-10-15 14:46:07 +0800

    Add release support unsafe

--- BinSerializer/WriterMethodAdapter.cs
+++ BinSerializer/WriterMethodAdapter.cs
@@ -5,19 +5,19 @@ using System.Security;
 namespace ThirtyNineEighty.BinarySerializer
 {
     internal sealed class WriterMethodAdapter<TFrom, TTo>
-  {
-    private readonly Writer<TFrom> _writer;
-
-    [SecurityCritical]
-    public WriterMethodAdapter(Delegate writer)
     {
-      _writer = (Writer<TFrom>)writer;
-    }
+        private readonly Writer<TFrom> _writer;
 
-    [SecuritySafeCritical]
-    public void Write(Stream stream, TTo value)
-    {
-      _writer(stream, (TFrom)(object)value);
+        [SecurityCritical]
+        public WriterMethodAdapter(Delegate writer)
+        {
+            _writer = (Writer<TFrom>)writer;
+        }
+
+        [SecuritySafeCritical]
+        public void Write(Stream stream, TTo value)
+        {
+            _writer(stream, (TFrom)(object)value);
+        }
     }
-  }
 }
